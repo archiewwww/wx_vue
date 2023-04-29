7e4f924eabb6fd1a1c3c2e430537406f62e53194
archiewwww <389214500@qq.com>
2023-04-29 18:44:36 +0400

    dsada

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,49 +5,6 @@
         width: 310px;
         height: 800px;
         flex-direction: column;
-        .search{
-            background-color: rgb(247, 247, 247);
-            width: 310px;
-            height: 80px;
-            justify-content: space-between;
-            padding: 30px 14px 0;
-            .left{
-                background-color: rgb(226, 226, 226);
-                width: 240px;
-                height: 30px;
-                border-radius: 8px;
-                flex-direction: row;
-                .iconfont{
-                    width: 20px;
-                    font-size: 20px;
-
-                }
-                input{
-                    background-color: transparent;
-                    /* margin-left: 20px; */
-                    width: 220px;
-                    height: 30px;
-                    border-radius: 8px;
-                    border: 0;
-                    outline: 0;
-/*                     &::placeholder{
-                        
-                    } */
-                }
-
-                input.aaa{
-                    color: red;
-                }
-            }
-            .right{
-                align-items: center;
-                justify-content: center;
-                background-color: rgb(226, 226, 226);
-                width: 30px;
-                height: 30px;
-                border-radius: 4px;
-            }
-        }
         .chat{
             background-color: plum;
             width: 310px;
